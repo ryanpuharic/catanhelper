947cd170d1ee6a33d8848b5df769e12c6f5eca35
Ryan Puharic <ryanpuharic@gmail.com>
2025-02-15 18:06:29 -0500

    new header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,11 +3,11 @@ import '../App.css';
 
 const Header = () => (
   <div className="header-container">
-    <div className="hexagon-corner hex-left" />
-    <div className="hexagon-corner hex-left2" />
-    <h1>CatanHelper</h1>
-    <div className="hexagon-corner hex-right" />
-    <div className="hexagon-corner hex-right2" />
+      <img 
+        src="./catanheader.png" 
+        alt="CatanHelper Logo" 
+        className="header-image"
+      />
   </div>
 );
 
